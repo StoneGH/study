73a4aed201d9c4cdb7192434335f76a0df243044
Stone <476896460@qq.com>
2019-12-29 17:46:14 +0800

    netty study

---
+++ study-netty/src/main/java/com/stone/study/netty/StudyNettyApplication.java
@@ -0,0 +1,13 @@
+package com.stone.study.netty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyNettyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyNettyApplication.class, args);
+    }
+
+}
---
+++ study-netty/src/test/java/com/stone/study/studynetty/StudyNettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.study.studynetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyNettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
