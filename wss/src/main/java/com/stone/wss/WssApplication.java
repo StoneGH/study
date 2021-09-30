f14bc2e9fea356a3e648892199c44a91a2b95b66
Stone <476896460@qq.com>
2021-09-30 15:48:04 +0800

    wss

---
+++ wss/src/main/java/com/stone/wss/WssApplication.java
@@ -0,0 +1,13 @@
+package com.stone.wss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WssApplication.class, args);
+    }
+
+}
---
+++ wss/src/test/java/com/stone/wss/WssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.wss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
